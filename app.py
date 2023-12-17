788b405f3b14f9d1195e0f9545cb31c02b882885
melbinmartin <79624780+melbinmartin@users.noreply.github.com>
2023-12-17 23:41:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, request, render_template, redirect, url_for
-from flask_mysqldb import MySQL
+import pymysql
+import pymysql.cursors
 
 app = Flask(__name__)
 
 # MySQL configurations
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Mysql221b@'
+app.config['MYSQL_HOST'] = 'database-2.cqjks2qufaje.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = '12345678'
 app.config['MYSQL_DB'] = 'Covid19'
 
-mysql = MySQL(app)
+def get_db_connection():
+    return pymysql.connect(host=app.config['MYSQL_HOST'],
+                           user=app.config['MYSQL_USER'],
+                           password=app.config['MYSQL_PASSWORD'],
+                           db=app.config['MYSQL_DB'],
+                           cursorclass=pymysql.cursors.DictCursor)
 
 @app.route('/')
 def index():
@@ -28,10 +34,12 @@ def submit():
         
         # validate received values
         if _state and _date and _samples and _deaths and _positive and _negative and _discharge:
-            cursor = mysql.connection.cursor()
+            conn = get_db_connection()
+            cursor = conn.cursor()
             cursor.execute("INSERT INTO Covid_details(State_Name, Date_of_Record, No_of_Samples, No_of_Deaths, No_of_Positive, No_of_Negative, No_of_Discharge) VALUES(%s, %s, %s, %s, %s, %s, %s)", (_state, _date, _samples, _deaths, _positive, _negative, _discharge))
-            mysql.connection.commit()
+            conn.commit()
             cursor.close()
+            conn.close()
             return redirect(url_for('index'))
         else:
             return 'Error: Missing form data. Please fill out all fields.', 400
@@ -41,13 +49,14 @@ def submit():
 
 @app.route('/cases')
 def cases():
-    cursor = mysql.connection.cursor()
+    conn = get_db_connection()
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM Covid_details ORDER BY No_of_Positive ASC")
     rows = cursor.fetchall()
     cursor.close()
+    conn.close()
 
     return render_template('cases.html', cases=rows)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
